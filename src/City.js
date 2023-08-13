5b56445fa72eed1950bc6a440c82114c254725e7
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-13 19:18:37 +0200

    Updated city style

--- src/City.js
+++ src/City.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./City.css";
 
 export default function City() {
-  return <h1> Vilnius</h1>;
+  return <h1>Rome</h1>;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
 import "./App.css";
-
 import "./index.css";
 
 const rootElement = document.getElementById("root");
